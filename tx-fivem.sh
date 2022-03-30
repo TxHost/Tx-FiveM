b79c4895118bb85c1275d5f4d8fb0b4e40b1efcb
TxHost <txhost@outlook.fr>
2022-03-30 19:50:54 +0100

    Update tx-fivem.sh

--- tx-fivem.sh
+++ tx-fivem.sh
@@ -176,16 +176,3 @@ printf "${CYAN} Démarrage de votre serveur Five M !"
     bash /home/fivem/run.sh +exec server.cfg +set txAdminPort 40120
 fi
 
-cat << "EOF"
-       _____    _   _           _   
-      |_   _|  | | | |         | |  
-        | |_  _| |_| | ___  ___| |_ 
-        | \ \/ /  _  |/ _ \/ __| __|
-        | |>  <| | | | (_) \__ \ |_ 
-        \_/_/\_\_| |_/\___/|___/\__|
-
-     Discord : https://discord.txhost.fr
-          Site : https://txhost.fr
-            
-            TxHost © 2021 - 2022      
-EOF
