c2c3672f7a07754ad0a8b227cd71607b26798fa0
TxHost <txhost@outlook.fr>
2022-03-31 17:53:15 +0100

    Commande tx-start

--- tx-start.sh
+++ tx-start.sh
@@ -1,4 +1,20 @@
 #!/bin/bash
 
+cat << "EOF"
+       _____    _   _           _   
+      |_   _|  | | | |         | |  
+        | |_  _| |_| | ___  ___| |_ 
+        | \ \/ /  _  |/ _ \/ __| __|
+        | |>  <| | | | (_) \__ \ |_ 
+        \_/_/\_\_| |_/\___/|___/\__|
+
+     Discord : https://discord.txhost.fr
+          Site : https://txhost.fr
+            
+            TxHost © 2021 - 2022                          
+EOF
+
+    sleep 2 
+
     cd /home/fivem
     bash /home/fivem/run.sh +exec server.cfg +set txAdminPort 40120
